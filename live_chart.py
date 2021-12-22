0557a177c838ecd75f7c81701b8351858e4a13ac
rongvrv <77297902+rongvrv@users.noreply.github.com>
2021-12-22 01:02:09 -0800

    click event for live chart

--- live_chart.py
+++ live_chart.py
@@ -7,6 +7,10 @@ from tos.helper import generate_sample_price_history
 import asyncio
 from threading import Thread
 import matplotlib.animation as animation
+from matplotlib import pyplot as plt
+plt.rcParams['keymap.save'].remove('s')
+plt.rcParams['keymap.fullscreen'].remove('f')
+
 
 TDSession = TDClient(
     credentials_path="C:\\AutoTrading\\tdameritrade_settings.json"
@@ -30,17 +34,37 @@ def start_streaming():
 thread = Thread(target=start_streaming)
 thread.start()
 
-color = mpf.make_marketcolors(up='#2E7D32',down='#D32F2F',inherit=True)
+color = mpf.make_marketcolors(up='#3A7153',down='#AC2E2E',inherit=True)
 style = mpf.make_mpf_style(base_mpf_style='yahoo', marketcolors=color)
 kwargs = dict(type='candle', volume=True, style=style)
 fig, axlist = mpf.plot(df, returnfig=True, **kwargs)
+
+def onclick(event):
+    print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+          ('double' if event.dblclick else 'single', event.button,
+           event.x, event.y, event.xdata, event.ydata))
+
+def onKeyPress(event):
+    print(event.__dict__)
+def onKeyRelease(event):
+    print(event)
+
+def onMouseMove(event):
+    print(event.__dict__)
+
+cid1 = fig.canvas.mpl_connect('button_press_event', onclick)
+cid2 = fig.canvas.mpl_connect('key_press_event', onKeyPress)
+cid3 = fig.canvas.mpl_connect('key_release_event', onKeyRelease)
+
+
 ax1 = axlist[0] # price
 ax2 = axlist[2] # volume
 def animate(i):
     ax1.clear()
     ax2.clear()
     kwargs2 = dict(type='candle', style=style)
-    mpf.plot(df,ax=ax1, volume=ax2, **kwargs2)
+    partial_df = df.iloc[-20:]
+    mpf.plot(partial_df,ax=ax1, volume=ax2, **kwargs2)
 
 ani = animation.FuncAnimation(fig, animate, interval=100)
 mpf.show()
